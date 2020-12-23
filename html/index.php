45ac00e4bf73df87be33f60a4d3098e6b91da864
[arash abolhasani] <[aras72h@gmail.com]>
2020-12-23 23:41:01 +0330

    starting mvc

--- html/index.php
+++ html/index.php
@@ -1,6 +1,6 @@
 <?php require_once('helpers.php'); ?>
 
-<?php render('header', 'title' => 'Home of PizzaML'); ?>
+<?php render('header', array('title' => 'Home of PizzaML')); ?>
 
     <h1>Welcome to PizzaML</h1>
     <h2><a href="menu.php">Order Now</a></h2>
