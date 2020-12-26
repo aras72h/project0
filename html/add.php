24e5774e19a2c3c34b4845383fb951871f282647
[arash abolhasani] <[aras72h@gmail.com]>
2020-12-26 17:53:05 +0330

    fixing html for product

--- html/add.php
+++ html/add.php
@@ -5,7 +5,13 @@
 <?php render('header', array('title' => 'cart')); ?>
 
 <h1>Shopping card</h1>
-<pre><?php print_r($_POST); ?></pre>
+
+<?php
+    
+    print_r($_POST);
+
+?>
 <a href="menu.php">Go back</a>
+<a href="#">Checkout</a>
 
 <?php render('footer'); ?>
--- html/product.php
+++ html/product.php
@@ -5,8 +5,12 @@
 <?php render('header', array('title' => 'product page')); ?>
     <h1> cheese Pizza </h1>
     <form action="add.php" method="post">
-        <input type="text" name="food">
-        <input type="submit" name="add" value="Add to Card">
+        <select name="food" id="food">
+            <option value="tomato">Tomato cheese</option>
+            <option value="onion">Onion</option>
+        </select>
+        <input type="number" min="1" max="10" step="1" value="1">
+        <input type="submut" value="Add to Cart">
     </form>
 
 <?php render('footer'); ?>
