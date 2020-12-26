3ce45502502200ca3e58030b1b2e752c0b09157e
[arash abolhasani] <[aras72h@gmail.com]>
2020-12-26 16:32:18 +0330

    adding not working

--- html/product.php
+++ html/product.php
@@ -5,7 +5,8 @@
 <?php render('header', array('title' => 'product page')); ?>
     <h1> cheese Pizza </h1>
     <form action="add.php" method="post">
-        <input type="button" name="add" value="Add to Card">
+        <input type="text" name="food">
+        <input type="submit" name="add" value="Add to Card">
     </form>
 
 <?php render('footer'); ?>
