3c3dc2705bb9910720057335921b1bd364a14074
[arash abolhasani] <[aras72h@gmail.com]>
2020-12-28 19:37:54 +0330

    testing the xml

--- html/index.php
+++ html/index.php
@@ -2,13 +2,17 @@
     session_start();
     require_once('helpers.php');
     $menu = simplexml_load_file('menu.xml');
+    $item_number = $menu->xpath('/ITEM['number']');
+    $name = $menu->xpath('/ITEM/NAME');
+    $size = $menu->xpath('/ITEM/SIZE');
 ?>
 
 <?php render('header', array('title' => 'Menu')); ?>
 
+<?php echo $item_number <br> $name <br> $size ?>
 <form action="cart.php" method="post">
-    <select name="menu" id="menu">
-    <option value="$number">$name $size</option>
+    <select name="item" id="item">
+    <option value="number">$name $size</option>
     </select>
     <input type="submit" value="Submit">
 </form>
